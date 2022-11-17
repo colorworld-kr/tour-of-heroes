96b321a0cd1a64102e6b99e94cdef78c4b8276d9
ks2lee <ks@colorworld.kr>
2022-11-18 00:49:22 +0900

    히어로 상세 조회시 '메시지 보내기' 추가하기 (+style 적용)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HEROES } from "../mock-heroes";
+
 import { HeroService } from "../hero.service";
+import { MessageService } from './../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -15,6 +18,7 @@ export class HeroesComponent implements OnInit {
 
   constructor(
     private heroService: HeroService,
+    private messageService: MessageService,
   ) { }
 
   ngOnInit(): void {
@@ -28,5 +32,6 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
