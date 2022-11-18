763e121d0d2ad1000f44a4ecb22556de5c51e7a4
mikey lee <ks@colorworld.kr>
2022-11-18 17:12:51 +0900

    Update dashboard.component.ts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService,
+  ) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(t_heroes => this.heroes = t_heroes.slice(1, 5));
+  }
 }
