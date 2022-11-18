ab184e467262d11301d3717025466e0b38121b3e
mikey lee <ks@colorworld.kr>
2022-11-19 00:01:34 +0900

    누락되었던 메시지 출력 로직 추가 (hero.service)

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { delay, Observable, of, tap } from "rxjs";
+import { delay, Observable, of, tap, catchError } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 
 import { Hero } from "./hero";
@@ -31,7 +31,11 @@ export class HeroService {
   ) { }
 
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')), // 기존과 동일하게 처리 데이터 응답 후 메시지 표시
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
@@ -42,5 +46,31 @@ export class HeroService {
       delay(800), // pipe, delay 코드 추가 : 실 서비스와 비슷하게 0.8초의 딜레이 적용
       tap(_ => this.messageService.add(`HeroService: fetched hero id=${id}`)), // tap 코드 보완 : 데이터 응답 후 메시지 표시
     );
+  }
+
+  /**
+ * HTTP 요청이 실패한 경우를 처리합니다.
+ * 애플리케이션 로직 흐름은 그대로 유지됩니다.
+ *
+ * @param operation - 실패한 동작의 이름
+ * @param result - 기본값으로 반환할 객체
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: 리모트 서버로 에러 메시지 보내기
+      console.error(error); // 지금은 콘솔에 로그를 출력합니다.
+
+      // TODO: 사용자가 이해할 수 있는 형태로 변환하기
+      this.log(`${operation} failed: ${error.message}`);
+
+      // 애플리케이션 로직이 끊기지 않도록 기본값으로 받은 객체를 반환합니다.
+      return of(result as T);
+    };
+  }
+
+  /** HeroService에서 보내는 메시지는 MessageService가 화면에 표시합니다. */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
