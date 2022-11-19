d667fc8bfc0d9686199bd67269fda159a049d998
mikey lee <ks@colorworld.kr>
2022-11-19 18:27:47 +0900

    ng generate component hero-search-new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroSearchNewComponent } from './hero-search-new/hero-search-new.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    HeroSearchNewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hero-search-new/hero-search-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-search-new',
+  templateUrl: './hero-search-new.component.html',
+  styleUrls: ['./hero-search-new.component.scss']
+})
+export class HeroSearchNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
