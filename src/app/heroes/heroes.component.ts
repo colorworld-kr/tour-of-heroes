fa830efaf629e545d9fda6822cf605d8a9060a02
mikey lee <ks@colorworld.kr>
2022-11-19 10:33:11 +0900

    (코드 복원) hero 직접 가져오는 방식으로 변경시 제거된 코드들

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HeroService } from "../hero.service";
+import { MessageService } from './../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,9 +12,11 @@ import { HeroService } from "../hero.service";
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
+  selectedHero?: Hero;
 
   constructor(
     private heroService: HeroService,
+    private messageService: MessageService,
   ) { }
 
   ngOnInit(): void {
@@ -23,5 +26,10 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(t_heroes => this.heroes = t_heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
