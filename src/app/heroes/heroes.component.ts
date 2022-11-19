21ae2954483b0b012726feda5ad1768d0983e6f9
mikey lee <ks@colorworld.kr>
2022-11-19 14:02:20 +0900

    (번외) 삭제 오류시 대응 코드 추가 (작동 확인 필요)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { catchError } from 'rxjs';
 
 import { Hero } from '../hero';
 import { HeroService } from "../hero.service";
@@ -43,7 +44,21 @@ export class HeroesComponent implements OnInit {
   }
 
   delete(hero: Hero): void {
+    let c_deleted_hero = hero;
     this.heroes = this.heroes.filter(t_h => t_h !== hero);
-    this.heroService.deleteHero(hero.id).subscribe();
+    this.heroService.deleteHero(hero.id)
+      .pipe(
+        catchError(_ => {
+          this.heroes.push(c_deleted_hero); // 삭제된 히어로 다시 삽입
+          this.heroes.sort((a, b) => a.id - b.id); // id 기준으로 다시 정렬
+          return [];
+        }) // 코드 의도 : api 오류 발생시 삭제된 데이터 복구 / 올바르게 작동하는지 확인 필요
+      ).subscribe();
+
+    // 삭제된 데이터 복구 로직 확인용 코드
+    // ).subscribe(_ => {
+    //   this.heroes.push(c_deleted_hero);
+    //   this.heroes.sort((a, b) => a.id - b.id);
+    // });
   }
 }
