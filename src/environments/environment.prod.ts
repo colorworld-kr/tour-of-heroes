c7869d329b87bdb378c911e3537cccb4517577d1
mikey lee <ks@colorworld.kr>
2022-12-09 01:07:58 +0900

    Update environment.prod.ts
    환경 설정값은 동일한 구조를 가지고 있어야 한다. (그렇지 않으면 컴파일 오류 발생함)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
-// import { firebaseConfig } from './firebase.prod'; // 아직 준비 중 / 비활성화
+import { firebaseConfig } from './firebase.prod'; // 아직 준비되지 않음 - 동일함.
 
 export const environment = {
   production: true,
-  // firebaseConfig,
+  firebaseConfig,
 };
