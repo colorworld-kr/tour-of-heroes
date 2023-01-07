9ade6692aca6411bb7f2126b01817dfd0c5dc1de
mikey lee <ks@colorworld.kr>
2023-01-07 18:10:00 +0900

    renamed:    src/app/app.module.ts -> src/app/modules/app.module.ts

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -8,17 +8,17 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore/';
 
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
-import { InMemoryDataService } from "./in-memory-data.service";
-import { environment } from '../environments/environment';
+import { InMemoryDataService } from "../in-memory-data.service";
+import { environment } from '../../environments/environment';
 
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './modules/app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroSearchComponent } from './hero-search/hero-search.component';
-import { HeroSearchNewComponent } from './hero-search-new/hero-search-new.component';
+import { AppComponent } from '../app.component';
+import { HeroesComponent } from '../heroes/heroes.component';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { MessagesComponent } from '../messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { HeroSearchComponent } from '../hero-search/hero-search.component';
+import { HeroSearchNewComponent } from '../hero-search-new/hero-search-new.component';
 
 @NgModule({
   declarations: [
